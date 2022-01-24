86d5422781f70eb0237a7cfabc54645348869eea
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2022-01-24 09:35:36 -0300

    refactor: centralize the authentication strategy, serialize and deserialize with passport library

--- playground/server/lib/passport/index.js
+++ playground/server/lib/passport/index.js
@@ -1,3 +1,5 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 /* eslint-disable no-unused-vars */
 const UserService = require('../../services/UserService');
 
@@ -6,5 +8,55 @@ const UserService = require('../../services/UserService');
  * @param {*} config
  */
 module.exports = (config) => {
-  //
+  passport.use(
+    new LocalStrategy(
+      {
+        passReqToCallback: true,
+        usernameField: 'username', // set the name that passport looks for the field in model User
+        passwordField: 'password', // line 13 and 14 shows the default values
+      },
+      async (req, username, password, done) => {
+        try {
+          const user = await UserService.findByUsername(req.body.username);
+          if (!user) {
+            req.session.messages.push({
+              text: 'Invalid username or password.',
+              type: 'danger',
+            });
+            return done(null, false);
+          }
+          if (user && !user.verified) {
+            req.session.messages.push({
+              text: 'Please verify your email address!',
+              type: 'danger',
+            });
+            return done(null, false);
+          }
+          const isValid = await user.comparePassword(req.body.password);
+          if (!isValid) {
+            req.session.messages.push({
+              text: 'Invalid username or password',
+              type: 'danger',
+            });
+            return done(null, false);
+          }
+          return done(null, user);
+        } catch (error) {
+          return done(error);
+        }
+      }
+    )
+  );
+  passport.serializeUser((user, done) => {
+    done(null, user.id); // serializatiion of the user
+  });
+  passport.deserializeUser(async (userId, done) => {
+    try {
+      const user = await UserService.findById(userId); // deserialization of the userId
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  });
+  return passport;
 };
